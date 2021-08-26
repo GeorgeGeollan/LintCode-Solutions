dd146f43ff3ee85b25f33b04eb34fba423d25d88
GeorgeGeollan <85476459+GeorgeGeollan@users.noreply.github.com>
2021-08-26 09:57:26 +0800

    solutions for 145 lowercaseToUppercase

---
+++ 145 lowercaseToUppercase.java
@@ -0,0 +1,12 @@
+public class Solution {
+    /**
+     * @param character: a character
+     * @return: a character
+     */
+    public char lowercaseToUppercase(char character) {
+        // write your code here
+        int upper = character - 32;
+
+        return (char)upper;
+    }
+}
